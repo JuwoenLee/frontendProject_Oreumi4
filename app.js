486fb3e106d6071e6db40568b62816c61030e416
LeeJuWon <woen1016@gmail.com>
2024-01-17 00:25:04 +0900

    project day2 - refactor

--- app.js
+++ app.js
@@ -1,67 +1,51 @@
-let mapContainer = document.getElementById('map'), // 지도를 표시할 div
+// kakao map api --------------------------------------------------------------
+
+let mapContainer = document.querySelector('.map-box'),
     mapOption = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
+        center: new kakao.maps.LatLng(33.4423379727783, 126.571449734542 ),
+        level: 3
     };
 
-// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 const map = new kakao.maps.Map(mapContainer, mapOption);
-// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
 const mapTypeControl = new kakao.maps.MapTypeControl();
-
-// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
-// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
-map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
-
-// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
 const zoomControl = new kakao.maps.ZoomControl();
-map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT)
+const geocoder = new kakao.maps.services.Geocoder();
+let marker = new kakao.maps.Marker();
 
-// 주소-좌표 변환 객체를 생성합니다
-var geocoder = new kakao.maps.services.Geocoder();
-
-var marker = new kakao.maps.Marker(), // 클릭한 위치를 표시할 마커입니다
-    infowindow = new kakao.maps.InfoWindow({zindex:1}); // 클릭한 위치에 대한 주소를 표시할 인포윈도우입니다
-
-// 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 searchAddrFromCoords(map.getCenter());
 
-// 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록합니다
 kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
     searchDetailAddrFromCoords(mouseEvent.latLng, function(result, status) {
         if (status === kakao.maps.services.Status.OK) {
-            // 마커를 클릭한 위치에 표시합니다
             marker.setPosition(mouseEvent.latLng);
             marker.setMap(map);
 
-            // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
             let infoDiv = document.querySelector('.top-map-place');
             infoDiv.innerHTML = result[0].address.address_name;
         }
     });
 });
 
-// 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
 kakao.maps.event.addListener(map, 'idle', function() {
     searchAddrFromCoords(map.getCenter());
 });
 
 function searchAddrFromCoords(coords, callback) {
-    // 좌표로 행정동 주소 정보를 요청합니다
     geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
 }
 
 function searchDetailAddrFromCoords(coords, callback) {
-    // 좌표로 법정동 상세 주소 정보를 요청합니다
     geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
 }
 
-
+// connecting with html --------------------------------------------------------------
 
 const subscribeBtn = document.querySelector(".btn-subscribe");
 const modalWindow = document.querySelector("#modal");
-const modalBtn = document.querySelector(".modal-btn");
-const gotoTopBtn = document.querySelector(".scroll-btn");
+const modalBtn = document.querySelector(".btn-modal");
+const gotoTopBtn = document.querySelector(".btn-back-to-top");
 const showMoreBtn = document.querySelector(".btn-show-more");
 const closeScrollBtn = document.querySelector(".btn-close-scroll");
 const imageList = document.querySelector(".infinite-scroll-list");
@@ -92,10 +76,6 @@ function makeImageList(datas) {
 }
 
 window.addEventListener('scroll', ()=>{
-    // 스크롤이 상단으로부터 얼마나 이동했는지 알아야합니다. (뷰포트의 높이 + 스크롤된 길이)
-    // 화면에 로딩된 페이지의 전체 높이
-    // 뷰포트의 높이 + 스크롤된 길이 + 10 === 화면에 로딩된 페이지의 전체 높이
-
     if(window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight * 0.8 && imageBox.style.display === "block"){
         fetchImages(pageToFetch++);
     }
@@ -113,10 +93,27 @@ closeScrollBtn.addEventListener('click', () => {
    closeScrollBtn.style.display = "none";
 });
 
-gotoTopBtn.addEventListener('click', () => {
-    if(window.scrollY !== 0) {
-        window.scrollTo(0, 0);
+window.addEventListener('scroll', () => {
+    if(document.documentElement.scrollTop > 150 ||
+        document.body.scrollTop > 150) {
+        document.querySelector('.btn-back-to-top').style.display = 'block';
+    } else {
+        document.querySelector('.btn-back-to-top').style.display = 'none';
     }
+})
+
+function backToTop() {
+    const position = document.documentElement.scrollTop || document.body.scrollTop;
+    if(position) {
+       window.requestAnimationFrame(() => {
+           window.scrollTo(0, position - position / 10);
+           backToTop();
+       });
+    }
+}
+
+gotoTopBtn.addEventListener('click', () => {
+    backToTop();
 });
 
 subscribeBtn.addEventListener('click', () => {
