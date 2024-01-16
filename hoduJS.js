88f94bf7031d993ba19db3e85a727c492c0b73d2
LeeJuWon <woen1016@gmail.com>
2024-01-16 18:38:11 +0900

    project day2 - feat

--- hoduJS.js
+++ hoduJS.js
@@ -1,3 +1,41 @@
+let mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+const map = new kakao.maps.Map(mapContainer, mapOption);
+// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+const mapTypeControl = new kakao.maps.MapTypeControl();
+
+// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+const zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT)
+
+// 지도를 클릭한 위치에 표출할 마커입니다
+const marker = new kakao.maps.Marker({
+    // 지도 중심좌표에 마커를 생성합니다
+    position: map.getCenter()
+});
+// 지도에 마커를 표시합니다
+marker.setMap(map);
+
+// 지도에 클릭 이벤트를 등록합니다
+// 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+
+    // 클릭한 위도, 경도 정보를 가져옵니다
+    const latlng = mouseEvent.latLng;
+
+    // 마커 위치를 클릭한 위치로 옮깁니다
+    marker.setPosition(latlng);
+});
+
 
 
 const subscribeBtn = document.querySelector(".btn-subscribe");
@@ -33,18 +71,20 @@ function makeImageList(datas) {
     });
 }
 
-showMoreBtn.addEventListener('click', () => {
-    imageBox.style.display = "block";
-    closeScrollBtn.style.display = "block";
-    window.addEventListener('scroll', ()=>{
-        // 스크롤이 상단으로부터 얼마나 이동했는지 알아야합니다. (뷰포트의 높이 + 스크롤된 길이)
-        // 화면에 로딩된 페이지의 전체 높이
-        // 뷰포트의 높이 + 스크롤된 길이 + 10 === 화면에 로딩된 페이지의 전체 높이
+window.addEventListener('scroll', ()=>{
+    // 스크롤이 상단으로부터 얼마나 이동했는지 알아야합니다. (뷰포트의 높이 + 스크롤된 길이)
+    // 화면에 로딩된 페이지의 전체 높이
+    // 뷰포트의 높이 + 스크롤된 길이 + 10 === 화면에 로딩된 페이지의 전체 높이
 
-        if(window.innerHeight + document.documentElement.scrollTop + 10 >= document.documentElement.offsetHeight){
-            fetchImages(pageToFetch++);
-        }
-    });
+    if(window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight * 0.8 && imageBox.style.display === "block"){
+        fetchImages(pageToFetch++);
+    }
+});
+
+showMoreBtn.addEventListener('click', () => {
+        imageBox.style.display = "block";
+        closeScrollBtn.style.display = "block";
+        fetchImages();
     }
 );
 
@@ -68,9 +108,3 @@ modalBtn.addEventListener('click', () => {
     console.log("modalBtn click");
     modalWindow.style.display = 'none';
 });
-
-
-fetchImages();
-
-
-
