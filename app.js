4e63a59eb3d5396ae61756d09df8dfa5375feb94
LeeJuWon <woen1016@gmail.com>
2024-01-22 13:34:21 +0900

    Revert "Feat:"
    This reverts commit 85d754ad02a4072cee9f491ee44c27ad686414bc.

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ const footerLogoImage = document.querySelector(".footer-hodu-logo");
 const exceptModal = document.querySelectorAll("body > *:not(#modal)");
 
 const homePosition = document.querySelector("#header"). getBoundingClientRect().top + window.scrollY;
-const mainPosition = document.querySelector("#main"). getBoundingClientRect().top + window.scrollY;
 const subscribePosition = document.querySelector(".subscribe-box"). getBoundingClientRect().top + window.scrollY;
 
 
@@ -39,6 +38,8 @@ async function fetchImages(){
 
 const infinityScroll = () => {
     if(document.body.clientHeight > (window.scrollY + window.innerHeight) * 0.5) {
+        console.log(document.body.clientHeight);
+        console.log((window.scrollY + window.innerHeight) * 0.5);
         fetchImages();
     }
 };
@@ -57,11 +58,9 @@ const throttling = (callback, delay) => {
 };
 
 function makeImageList(datas) {
-    if(imageBox.style.display === "block") {
-        datas.forEach((item) => {
-            imageList.innerHTML += "<img src=" + item.url + " alt=''>";
-        });
-    }
+    datas.forEach((item) => {
+        imageList.innerHTML += "<img src=" + item.url + " alt=''>";
+    });
 }
 
 navHome.addEventListener('click', () => {
